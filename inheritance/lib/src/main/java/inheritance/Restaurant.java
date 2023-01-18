5f4360b82143f6c1c3bb3725a337fd46a8ea1556
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2023-01-17 23:20:49 -0800

    Added some code

---
+++ inheritance/lib/src/main/java/inheritance/Restaurant.java
@@ -0,0 +1,23 @@
+package inheritance;
+
+public class Restaurant {
+    private Double stars;
+    private String price;
+    private String name;
+
+    public Restaurant() {
+    }
+
+    public Restaurant(Double stars, String price, String name) {
+    this.stars;
+    this.price;
+    this.name;
+    }
+
+    //Methods
+    addReview(Review review) {
+        System.out.println();
+    }
+}
+
+
