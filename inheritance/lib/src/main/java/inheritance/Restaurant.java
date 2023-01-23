1ce770a838b6ea68a8394db884d21b2882048418
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2023-01-22 23:40:29 -0800

    Finished feature tasks and updated Readme

--- inheritance/lib/src/main/java/inheritance/Restaurant.java
+++ inheritance/lib/src/main/java/inheritance/Restaurant.java
@@ -19,15 +19,15 @@ public class Restaurant {
     //Methods
     public void addReview(Review review) {
         int sumOfReviewStars = 0;
-        // 1. Add it to reviews List
+
         reviews.add(review);
-        // 2. iterate over and get total sum of values
+
         for (Review arrayReview : reviews) {
             sumOfReviewStars += arrayReview.getStars();
         }
-        // 3. calculate averageNum of stars
+
         float averageNumOfStars = (sumOfReviewStars / reviews.size());
-        // 4. call setNumOfStars with new value
+
         setStars(averageNumOfStars);
     }
 
